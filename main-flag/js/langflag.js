f74ca3149c5eb94db01e5b740dcb06285fcd8c41
Skapar <skaparadr@gmail.com>
2023-08-26 12:45:01 +0600

    commit

---
+++ main-flag/js/langflag.js
@@ -0,0 +1,23 @@
+let flags = ['en', 'kz', 'ru'];
+let currentIndex = 0;
+
+function switchFlag(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) currentIndex = flags.length - 1;
+    if (currentIndex >= flags.length) currentIndex = 0;
+    document.getElementById("flag").src = "flags-gif/" + flags[currentIndex] + ".gif";
+}
+
+function redirectToPage() {
+    switch (flags[currentIndex]) {
+        case 'en':
+            window.location.href = 'redirected-page.html';
+            break;
+        case 'kz':
+            window.location.href = 'kz_page.html';
+            break;
+        case 'ru':
+            window.location.href = 'ru_page.html';
+            break;
+    }
+}
